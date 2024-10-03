e0bb20e32a176910725b7ef9d8ff39e55c1ec6a6
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2024-10-04 08:53:56 +1300

    Shooting
    The player is now able to attack by shooting cubes around.

---
+++ Scripts/Gun.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class Gun : Node2D{
+	[Export] PackedScene bulletScene;
+	[Export] float bulletSpeed = 700f;
+	[Export] float bps = 5;
+	[Export] float bulletDamage = 2f;
+	
+	float fireRate;
+	float attackCooldown = 0f;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready(){
+		fireRate = 1 / bps;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta){
+		if (Input.IsActionPressed("shoot") && attackCooldown > fireRate){
+			RigidBody2D bullet = bulletScene.Instantiate<RigidBody2D>();
+			
+			bullet.Rotation = GlobalRotation;
+			bullet.GlobalPosition = GlobalPosition;
+			bullet.LinearVelocity = bullet.Transform.X * bulletSpeed;
+			
+			GetTree().Root.AddChild(bullet);
+			
+			attackCooldown = 0f;
+		}
+		else{
+			attackCooldown += (float)delta;
+		}
+	}
+}
