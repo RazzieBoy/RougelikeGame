47bb8744fc500a541c34977f831829876516bb07
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2024-10-09 12:27:37 +1300

    Reload function
    Trying to make a reload function on a timer. Not going super well but i am not done yet.

--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -8,27 +8,53 @@ public partial class Gun : Node2D{
 	[Export] float bps = 5;
 	[Export] float bulletDamage = 2f;
 	
+	float reload = 10;
+	bool isReloading = false;
 	float fireRate;
 	float attackCooldown = 0f;
+	float ReloadTime = 2f;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
 		fireRate = 1 / bps;
+		//Timer reloadTimer = GetNode<Timer>("ReloadTimer");
+		//reloadTimer.Connect("timeout", this, "_on_timer_timeout");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta){
 		if (Input.IsActionPressed("shoot") && attackCooldown > fireRate){
-			RigidBody2D bullet = bulletScene.Instantiate<RigidBody2D>();
+			if (!isReloading && reload > 0){
+				RigidBody2D bullet = bulletScene.Instantiate<RigidBody2D>();
 			
-			bullet.Rotation = GlobalRotation;
-			bullet.GlobalPosition = GlobalPosition;
-			bullet.LinearVelocity = bullet.Transform.X * bulletSpeed;
-			GetTree().Root.AddChild(bullet);
-			attackCooldown = 0f;
+				bullet.Rotation = GlobalRotation;
+				bullet.GlobalPosition = GlobalPosition;
+				bullet.LinearVelocity = bullet.Transform.X * bulletSpeed;
+				GetTree().Root.AddChild(bullet);
+				attackCooldown = 0f;
+			}
+			reload--;
+			if	(reload <= 0){
+				StartReload();
+			}
 		}
 		else{
 			attackCooldown += (float)delta;
 		}
+	}
+	
+	private void StartReload(){
+		isReloading = true;
+		reload = 10;
+		Timer timer = new Timer();
+		AddChild(timer);
+		timer.WaitTime = ReloadTime;
+		timer.OneShot = true;
+		timer.Connect("timeout", this, nameof(OnReloadComplete));
+		timer.Start();
+	}
+	
+	private void OnReloadComplete(){
+		isReloading = false;
 	}
 }
