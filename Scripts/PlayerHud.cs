77398ce211aecd05e772a2b5c8bcf59ea53e600f
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2024-10-30 12:18:56 +1300

    Design Changes
    made the player hud look a bit nicer

--- Scripts/PlayerHud.cs
+++ Scripts/PlayerHud.cs
@@ -8,10 +8,10 @@ public partial class PlayerHud : Control{
 	private Label primaryCooldownLabel;
 
 	public override void _Ready(){
-		primaryAmmoLabel = GetNode<Label>("Primary");
-		secondaryAmmoLabel = GetNode<Label>("Secondary");
-		dashCooldownLabel = GetNode<Label>("Utility");
-		primaryCooldownLabel = GetNode<Label>("Special");
+		primaryAmmoLabel = GetNode<Label>("PrimaryBackground/Primary");
+		secondaryAmmoLabel = GetNode<Label>("SecondaryBackground/Secondary");
+		dashCooldownLabel = GetNode<Label>("UtilityBackground/Utility");
+		primaryCooldownLabel = GetNode<Label>("SpecialBackground/Special");
 	}
 
 	// Method to update ammo display, called by Main.cs
@@ -24,11 +24,11 @@ public partial class PlayerHud : Control{
 	}
 	
 	 public void UpdateDashCooldownDisplay(object sender, float cooldownTime){
-		dashCooldownLabel.Text = $"{cooldownTime:F1}s";  // Dash cooldown
+		dashCooldownLabel.Text = $"{cooldownTime:F0}";  // Dash cooldown
 	}
 	
 	public void UpdateCooldownDisplayPrimary(object sender, float cooldownTime){
-		primaryCooldownLabel.Text = $"{cooldownTime:F1}s";  // Primary cooldown only
+		primaryCooldownLabel.Text = $"{cooldownTime:F0}";  // Primary cooldown only
 	}
 	
 	public override void _Process(double delta){
